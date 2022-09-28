7b395a1f370358c5a099ddb10e8e31c0ec5a7489
Caza20 <matias.cazanave@gmail.com>
2022-09-28 14:42:51 -0300

    Tarea JSIII resuelta

--- 04-JS-III/homework/tests/JSIII.test.js
+++ 04-JS-III/homework/tests/JSIII.test.js
@@ -151,10 +151,10 @@ describe('empiezaConNueve(n))', function() {
 
 describe('todosIguales(arreglo)', function() {
   it('should return true if all the elements of the array are equal and false otherwise', function() {
-    expect(todosIguales([20, 20, 20, 20 ])).toBe(true);
+    expect(todosIguales([20, 20, 20, 20])).toBe(true);
     expect(todosIguales([97, 100, 190, 9])).toBe(false);
    
-    expect(todosIguales([230, 230, 230, 230 ])).toBe(true);
+    expect(todosIguales([230, 230, 230, 230])).toBe(true);
   });
 });
 
